828b7535f3f504ebc7da3467740320ac052090ba
mercepluka <luka@calimero.network>
2023-06-07 15:15:46 +0200

    Added enum partialeq

--- core/primitives-core/src/config.rs
+++ core/primitives-core/src/config.rs
@@ -565,7 +565,7 @@ impl ExtCosts {
     }
 }
 
-#[derive(Clap, Clone, Debug, PartialEq)]
+#[derive(Clap::Parser, Clone, Debug, PartialEq)]
 pub enum PatchGenesisConfig {
     /// Genesis config override with values from patch file.
     Patch = 0,
