0256113c1cd9c3c976cd8ae75bc1bc51cccea2ac
mercepluka <luka@calimero.network>
2023-06-13 18:04:06 +0200

    Added patching

--- core/chain-configs/src/lib.rs
+++ core/chain-configs/src/lib.rs
@@ -1,16 +1,17 @@
 mod client_config;
+mod genesis_config_patch;
 mod genesis_config;
 pub mod genesis_validate;
 #[cfg(feature = "metrics")]
 mod metrics;
 mod updateable_config;
-mod genesis_config_patch;
 
 pub use client_config::{
     ClientConfig, DumpConfig, ExternalStorageConfig, ExternalStorageLocation, GCConfig,
     LogSummaryStyle, StateSyncConfig, SyncConfig, DEFAULT_GC_NUM_EPOCHS_TO_KEEP,
     MIN_GC_NUM_EPOCHS_TO_KEEP, TEST_STATE_SYNC_TIMEOUT,
 };
+pub use genesis_config_patch::GenesisConfigPatch;
 pub use genesis_config::{
     get_initial_supply, stream_records_from_file, Genesis, GenesisChangeConfig, GenesisConfig,
     GenesisRecords, GenesisValidationMode, ProtocolConfig, ProtocolConfigView,
